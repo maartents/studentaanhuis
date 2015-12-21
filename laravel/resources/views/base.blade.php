e27f9cba119c5302e5cef69d5b83c967dc5d3109
Maarten de Wolf <pilootwolfje@hotmail.com>
2015-12-21 15:13:45 +0100

    commit some adds

---
+++ laravel/resources/views/base.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>StudentAanHuis BE</title>
+    <link rel="stylesheet" href="http://bootswatch.com/yeti/bootstrap.min.css"/>
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+</head>
+<body>
+
+</body>
+</html>
